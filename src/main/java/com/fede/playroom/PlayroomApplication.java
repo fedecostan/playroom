e08ff148e280ac6801b5ed7f160d5b2b78033b0d
fedecostan <costantino.fe@gmail.com>
2023-12-21 21:07:15 +0100

    Initial commit - Spring boot application

---
+++ src/main/java/com/fede/playroom/PlayroomApplication.java
@@ -0,0 +1,13 @@
+package com.fede.playroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fede/playroom/PlayroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fede.playroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
